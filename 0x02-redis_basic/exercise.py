b103580a623b49e6f0391afdc69217272f133a45
Eloagbawe <eloagbawe@gmail.com>
2023-02-16 00:04:28 +0100

    add solution to task 0

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This file contains the cache class"""
+import redis
+from typing import Union
+import uuid
+
+
+class Cache:
+    """This represents a cache class"""
+    def __init__(self):
+        """initialization"""
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[int, str, bytes, float]) -> str:
+        """store method"""
+        random_string = str(uuid.uuid4())
+        self._redis.set(random_string, data)
+        return random_string
